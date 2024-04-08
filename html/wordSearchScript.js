b7138e56a41e7e077c3a50b984ee8c42db234e24
Ashish Rupani <ayr9092@mavs.uta.edu>
2024-04-08 13:54:38 -0500

    Added html files for each page, navigation works

--- html/wordSearchScript.js
+++ html/wordSearchScript.js
@@ -5,7 +5,7 @@
 // serverUrl = "ws://" + window.location.hostname + ":9880";
 // connection = new WebSocket(serverUrl);
 
-let nick;
+let nick;   //Players unique nick
 
 function nameFunction() {   //gets the username 
     nick = document.querySelector("#nick");
@@ -16,5 +16,24 @@ function nameFunction() {   //gets the username
     }
     else {
         document.getElementById("errorMsg").innerHTML = "Error: Please enter a proper username.";
+    }
+}
+
+function roomFunction(){ //Navigate to room page
+    window.location.assign("wordSearchRoom.html");
+}
+
+function backToNameFunction(){ //Navigate to room page
+    window.location.assign("wordSearchName.html");
+}
+function backToLobbyFunction(){ //Navigate to room page
+    window.location.assign("wordSearchLobby.html");
+}
+
+window.onload = function(){
+    for (let node of document.querySelectorAll("td")) {
+        if(node.textContent != "") continue;
+        let charCode = Math.round(65 + Math.random() * 25)
+        node.textContent = String.fromCharCode(charCode);
     }
 }
