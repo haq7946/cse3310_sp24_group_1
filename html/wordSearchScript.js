3a296234786281348106faaef84fd97dfd149f1b
ekinsand <aekins@smu.edu>
2024-04-15 22:28:05 -0500

    added ashish's code

--- html/wordSearchScript.js
+++ html/wordSearchScript.js
@@ -5,41 +5,55 @@ class Player {
     status;
     numberOfVictories;
 }   //Players unique nick
-
-
+ 
+class Lobby {
+    gameList
+    playerList
+    //playerChat
+}
+ 
+ 
 var connection = null;
 var serverUrl;
 serverUrl = "ws://" + window.location.hostname + ":9101";
 //Create the connection with the server
 connection = new WebSocket(serverUrl);
 console.log(connection);
-
+ 
+globalLobby = new Lobby();
+globalLobby.playerList = new Array();
+globalLobby.gameList = new Array();
+//globalLobby.playerChat = new Array();
+ 
 connection.onopen = function (evt) {
     console.log("open");
 }
 connection.onclose = function (evt) {
     console.log("close");
 }
-
+ 
 const BoardStatetoDisplay = new Map(); //Eventually will be used for the 50 by 50 grid
-
+ 
 connection.onmessage = function (evt){
     var msg;
     msg = evt.data;
         console.log("Message received: " + msg);
         const obj = JSON.parse(msg);
-
+   
+ 
 }
-
-
+ 
+ 
 ////////////////////////////////////////////////////
 var namePage = document.getElementById("namePage"); //Main page
 var lobbyPage = document.getElementById("lobbyPage"); //Lobby Page
 var roomPage = document.getElementById("roomPage"); //Game Page
 ////////////////////////////////////////////////////
 document.getElementById("rmButton").style.display = 'none'; ///Room button
-var display = 0;
-
+var display = 0;                                            //Debug purposes and control
+//////////////////////////////////////////////////
+var createRoomButton = document.getElementById("createRoom");  //Create Room
+ 
 function hideShow() {
     if (display == 0) {
         lobbyPage.style.display = 'none';
@@ -55,14 +69,14 @@ function hideShow() {
         namePage.style.display = 'none';
         lobbyPage.style.display = 'none';
         roomPage.style.display = 'block';
-
+ 
     }
 }
-
-function nameFunction() {   //gets the username 
+ 
+function nameFunction() {   //gets the username
     Player.username = document.querySelector("#nick");
     console.log(Player.username.value);
-
+ 
     if (Player.username.value != "") {
         //Send data to back-end port 9101
         P = new Player();
@@ -71,19 +85,21 @@ function nameFunction() {   //gets the username
         P.color = "0";
         P.status = "0";  //0 = notready and 1 = ready //
         P.numberOfVictories = "0";
-        connection.send(JSON.stringify(P));
-        console.log(JSON.stringify(P))
-
+ 
+        globalLobby.playerList.push(P);
+        connection.send(JSON.stringify(globalLobby));
+        console.log(JSON.stringify(globalLobby))
+ 
         display = 1;  //navigates user to next page
         hideShow();
     }
-    else 
+    else
     {
         document.getElementById("errorMsg").innerHTML = "Error: Please enter a proper username.";
         console.log("User didn't specify name");
     }
 }
-
+ 
 function backToNameFunction() { //Navigate to room page
     console.log(Player.username.value + " left the game");
     Player.username = 'none';
@@ -94,13 +110,13 @@ function backToNameFunction() { //Navigate to room page
     display = 0;
     hideShow();
 }
-
+ 
 function backToLobbyFunction() { //Navigate to room page
     display = 1;
     console.log(Player.username.value + " left the room");
     hideShow();
 }
-
+ 
 function roomFunction() { //Navigate to room page
     display = 2;
     console.log(Player.username.value + " joined the room");
@@ -109,22 +125,33 @@ function roomFunction() { //Navigate to room page
     console.log(JSON.stringify(P));
     hideShow();
 }
-
+ 
 window.onload = function () {
     hideShow();
-
+   
 }
-
-
+ 
+ 
 function createRoom() {
     // document.getElementById("room1").textContent = `${Player.nick.value}'s Room`;
     console.log(Player.username.value + " created a room");
     buildRooms();
+    disableRoomButton();
 }
-
-function buildRooms(){ //Add player data
+ 
+function disableRoomButton()
+{
+    createRoomButton.style.display = 'none';
+}
+ 
+function enableRoomButton()
+{
+    createRoomButton.style.display = 'block';
+}
+ 
+function buildRooms(){ //Add player data (will eventually print data based on JSON data)
     var table = document.getElementById("rmTable")
-
+ 
     for(var i = 0; i < 1; i++)
     {
         var row = `<tr>
@@ -133,13 +160,13 @@ function buildRooms(){ //Add player data
                   <tr />`
         table.innerHTML += row;
     }
-
+ 
     document.getElementById("rmButton").style.display = 'block';  //Display the room button
 }
-
+ 
 function buildLeaderBoard(){
     var leaderboard = document.getElementById("leaderboard");
-
+ 
     for(var i = 0; i < 1; i++)
     {
         var leaderBoardRow = `<tr>
@@ -148,18 +175,16 @@ function buildLeaderBoard(){
         leaderboard.innerHTML += leaderBoardRow;
     }
 }
-
-
-
+ 
+ 
+ 
 //////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////ROOOM//////////////////////////////////////////////////////////////
-
+ 
 const WIDTH = 50;
 const HEIGHT = 50;
 const Buttons = new Array(WIDTH*HEIGHT);
-let selected_letters = "";
-let idx =  Math.round(0 + Math.random() * 4); 
-const PlayerToColor = new Map([[0,"red"],[1,"orange"],[2,"green"],[3,"black"]]);
+let selected_letters = "";  
     for(let index=0;index<Buttons.length;index++) {
         let charCode = Math.round(65 + Math.random() * 25);
         Buttons[index]=String.fromCharCode(charCode);
@@ -177,18 +202,23 @@ const PlayerToColor = new Map([[0,"red"],[1,"orange"],[2,"green"],[3,"black"]]);
        let x = id % WIDTH;
        let y = Math.floor(id / HEIGHT);
        const letter = document.getElementById(id).innerHTML;
-       selected_letters += letter 
+       selected_letters += letter
        document.getElementById("w3review").value="selected "+letter+" at coordinate ("+x+","+y+")\nselected letters="+selected_letters;
        let bcolor = document.getElementById(id).style.backgroundColor;
-
-       if(bcolor == PlayerToColor.get(idx))
+       if(bcolor == "orange")
           document.getElementById(id).style.backgroundColor = "blue";
        else
-          document.getElementById(id).style.backgroundColor = PlayerToColor.get(idx);
-
+          document.getElementById(id).style.backgroundColor = "black";
     }
-
-
+ 
+    function saveToFile() {
+/*
+        const fs = require('node:fs');
+        fs.writeFileSync("data.txt", JSON.stringify(Buttons));
+ 
+*/
+    }
+ 
     function updateButtons(_Buttons,sButtons) {
        for(let i=0;i<_Buttons.length;i++)
            _Buttons[i] = sButtons[i];
--- src/main/java/uta/cse3310/App.java
+++ src/main/java/uta/cse3310/App.java
@@ -1,16 +1,15 @@
-
 // This is example code provided to CSE3310 Fall 2022
 // You are free to use as is, or changed, any of the code provided
-
+ 
 // Please comply with the licensing requirements for the
 // open source packages being used.
-
+ 
 // This code is based upon, and derived from the this repository
 //            https:/thub.com/TooTallNate/Java-WebSocket/tree/master/src/main/example
-
+ 
 // http server include is a GPL licensed package from
 //            http://www.freeutils.net/source/jlhttp/
-
+ 
 /*
  * Copyright (c) 2010-2020 Nathan Rajlich
  *
@@ -35,179 +34,186 @@
  *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  *  OTHER DEALINGS IN THE SOFTWARE.
  */
-
-package uta.cse3310;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.util.Collections;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.Vector;
-import java.time.Instant;
-import java.time.Duration;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.util.ArrayList;
-
-public class App extends WebSocketServer
-{
-
-  private int connectionId = 0;
-  private Lobby myLobby;
-
-  public App(int port) 
-  {
-    super(new InetSocketAddress(port));
-    myLobby = new Lobby();
-  }
-
-  public static void main(String[] args) 
-  {
-    String HttpPort = System.getenv("HTTP_PORT");
-    int port = 9001;
-    if (HttpPort!=null) 
-    {
-      port = Integer.valueOf(HttpPort);
-    } 
-    // Set up the http server
-    HttpServer H = new HttpServer(port, "./html");
-    H.start();
-    System.out.println("http Server started on port: " + port);
-
-    // create and start the websocket server
-    port = 9101;
-    String WSPort = System.getenv("WEBSOCKET_PORT");
-    if (WSPort!=null)
-    {
-      port = Integer.valueOf(WSPort);
-    }
-
-    App myApp = new App(port);
-    myApp.setReuseAddr(true);
-    myApp.start();
-    System.out.println("websocket Server started on port: " + port);
-
-    //code below is for testing only
-    /*myApp.makeGame();
-    myApp.addPlayer("test player 1", 1);
-    myApp.addPlayer("test player 2", 2);
-    for(Game i : gameList)
-    {
-      i.displayInfo();
-    }
-    for(Player i : playerList)
-    {
-      System.out.println("username = " + i.getUsername());
-    }
-    myApp.joinGame(gameList.get(0), playerList.get(0));
-
-    for(Game i : gameList)
-    {
-      i.displayInfo();
-    }
-    for(Player i : playerList)
-    {
-      System.out.println("username = " + i.getUsername());
-    }*/
-  }
-
-  @Override
-  public void onOpen(WebSocket conn, ClientHandshake handshake) 
-  {
-    System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " connected");
-
-    //ServerEvent E = new ServerEvent();  Our server event
-
-    // No matches ? Create a new Game.
-    Lobby L = new Lobby();
-    // allows the websocket to give us the Game when a message arrives
-    conn.setAttachment(L);
-
-    Gson gson = new Gson();
-    // Note only send to the single connection
-    conn.send(gson.toJson(L));
-    System.out.println(gson.toJson(L));
-    System.out.println("This is a test");
-
-    // The state of the game has changed, so lets send it to everyone
-    String jsonString;
-    jsonString = gson.toJson(L);
-
-    System.out.println(jsonString);
-    broadcast(jsonString);
-  }
-
-  @Override
-  public void onClose(WebSocket conn, int code, String reason, boolean remote) 
-  {
-    System.out.println(conn + " has closed");
-    // Retrieve the game tied to the websocket connection
-    Lobby L = conn.getAttachment();
-    L = null;
-  }
-
-  @Override
-  public void onMessage(WebSocket conn, String message) 
-  {
-    //System.out.println("< " + Duration.between(startTime, Instant.now()).toMillis() + " " + "-" + " " + escape(message));
-
-    // Bring in the data from the webpage
-    // A UserEvent is all that is allowed at this point
-    GsonBuilder builder = new GsonBuilder();
-    Gson gson = builder.create();
-    Player P = gson.fromJson(message, Player.class);
-    System.out.println("Have I got here?");
-    System.out.println(P.getUsername());
-
-    UserEvent U = new UserEvent();
-    // Get our Game Object
-    ///p
-    Player C = conn.getAttachment();
-    //L.updateState(U);
-
-    // send out the game state every time
-    // to everyone
-    String jsonString;
-    jsonString = gson.toJson(P);
-
-    //System.out.println("> " + Duration.between(startTime, Instant.now()).toMillis() + " " + "*" + " " + escape(jsonString));
-    System.out.println(jsonString);
-    broadcast(jsonString);
-  }
-
-  @Override
-  public void onMessage(WebSocket conn, ByteBuffer message) 
-  {
-    System.out.println(conn + ": " + message);
-  }
-
-  @Override
-  public void onError(WebSocket conn, Exception ex) 
-  {
-    ex.printStackTrace();
-    if (conn != null) {
-      // some errors like port binding failed may not be assignable to a specific
-      // websocket
-    }
-  }
+ 
+ package uta.cse3310;
+ 
+ import java.io.BufferedReader;
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ import java.net.InetSocketAddress;
+ import java.net.UnknownHostException;
+ import java.nio.ByteBuffer;
+ import java.util.Collections;
   
-  @Override
-  public void onStart() 
-  {
-    System.out.println("Server started!");
-    setConnectionLostTimeout(0);
-  }
-}
+ import org.java_websocket.WebSocket;
+ import org.java_websocket.drafts.Draft;
+ import org.java_websocket.drafts.Draft_6455;
+ import org.java_websocket.handshake.ClientHandshake;
+ import org.java_websocket.server.WebSocketServer;
+ import java.util.Timer;
+ import java.util.TimerTask;
+ import java.util.Vector;
+ import java.time.Instant;
+ import java.time.Duration;
+  
+ import com.google.gson.Gson;
+ import com.google.gson.GsonBuilder;
+  
+ import java.util.ArrayList;
+  
+ public class App extends WebSocketServer
+ {
+  
+   private int connectionId = 0;
+   private Lobby myLobby;
+  
+   public App(int port)
+   {
+     super(new InetSocketAddress(port));
+     myLobby = new Lobby();
+   }
+  
+   public static void main(String[] args)
+   {
+     String HttpPort = System.getenv("HTTP_PORT");
+     int port = 9001;
+     if (HttpPort!=null)
+     {
+       port = Integer.valueOf(HttpPort);
+     }
+     // Set up the http server
+     HttpServer H = new HttpServer(port, "./html");
+     H.start();
+     System.out.println("http Server started on port: " + port);
+  
+     // create and start the websocket server
+     port = 9101;
+     String WSPort = System.getenv("WEBSOCKET_PORT");
+     if (WSPort!=null)
+     {
+       port = Integer.valueOf(WSPort);
+     }
+  
+     App myApp = new App(port);
+     myApp.setReuseAddr(true);
+     myApp.start();
+     System.out.println("websocket Server started on port: " + port);
+  
+     //code below is for testing only
+     /*myApp.makeGame();
+     myApp.addPlayer("test player 1", 1);
+     myApp.addPlayer("test player 2", 2);
+     for(Game i : gameList)
+     {
+       i.displayInfo();
+     }
+     for(Player i : playerList)
+     {
+       System.out.println("username = " + i.getUsername());
+     }
+     myApp.joinGame(gameList.get(0), playerList.get(0));
+  
+     for(Game i : gameList)
+     {
+       i.displayInfo();
+     }
+     for(Player i : playerList)
+     {
+       System.out.println("username = " + i.getUsername());
+     }*/
+   }
+  
+   @Override
+   public void onOpen(WebSocket conn, ClientHandshake handshake)
+   {
+     System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " connected");
+  
+     //ServerEvent E = new ServerEvent();  Our server event
+  
+     // No matches ? Create a new Game.
+     LobbyEvent L = new LobbyEvent();
+     L.gameList = new ArrayList<Game>();
+     L.playerList = new ArrayList<Player>();
+     // allows the websocket to give us the Game when a message arrives
+     conn.setAttachment(L);
+  
+     Gson gson = new Gson();
+     // Note only send to the single connection
+     conn.send(gson.toJson(L));
+     System.out.println(gson.toJson(L));
+     System.out.println("This is a test");
+  
+     // The state of the game has changed, so lets send it to everyone
+     String jsonString;
+     jsonString = gson.toJson(L);
+  
+     System.out.println(jsonString);
+     broadcast(jsonString);
+   }
+  
+   @Override
+   public void onClose(WebSocket conn, int code, String reason, boolean remote)
+   {
+     System.out.println(conn + " has closed");
+     // Retrieve the game tied to the websocket connection
+     LobbyEvent L = new LobbyEvent();
+     L.gameList = new ArrayList<Game>();
+     L.playerList = new ArrayList<Player>();
+     L = conn.getAttachment();
+     L = null;
+   }
+  
+   @Override
+   public void onMessage(WebSocket conn, String message)
+   {
+     //System.out.println("< " + Duration.between(startTime, Instant.now()).toMillis() + " " + "-" + " " + escape(message));
+  
+     // Bring in the data from the webpage
+     // Lobby Event is passed from Json to gson [Web to code]
+     GsonBuilder builder = new GsonBuilder();
+     Gson gson = builder.create();
+     LobbyEvent L = new LobbyEvent();
+     L.gameList = new ArrayList<Game>();
+     L.playerList = new ArrayList<Player>();
+     L = gson.fromJson(message, LobbyEvent.class);
+     //System.out.println("Have I got here?");
+     System.out.println(L);
+  
+  
+     // Get our Lobby Event data from server
+     LobbyEvent C = conn.getAttachment();
+     //L.updateState(C); we update here
+  
+     // send out the game state every time
+     // to everyone
+     String jsonString;
+     jsonString = gson.toJson(L);
+  
+     //System.out.println("> " + Duration.between(startTime, Instant.now()).toMillis() + " " + "*" + " " + escape(jsonString));
+     System.out.println(jsonString);
+     broadcast(jsonString);
+   }
+  
+   @Override
+   public void onMessage(WebSocket conn, ByteBuffer message)
+   {
+     System.out.println(conn + ": " + message);
+   }
+  
+   @Override
+   public void onError(WebSocket conn, Exception ex)
+   {
+     ex.printStackTrace();
+     if (conn != null) {
+       // some errors like port binding failed may not be assignable to a specific
+       // websocket
+     }
+   }
+  
+   @Override
+   public void onStart()
+   {
+     System.out.println("Server started!");
+     setConnectionLostTimeout(0);
+   }
+ }
